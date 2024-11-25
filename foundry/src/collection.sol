711aedffeae35198cc1205425e56d5cf41822314
gaetan cabaret <gaetan@e36knots.com>
2024-11-25 16:19:12 +0100

    fix(struct)

--- foundry/src/collection.sol
+++ foundry/src/collection.sol
@@ -2,6 +2,9 @@
 
 pragma solidity ^0.8.23;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+
 // create erc-721 implementation pointed by a proxy
 contract ERC721Implementation is ERC721 {
     constructor() ERC721("ERC721Implementation", "ERC721IMPL") {}
