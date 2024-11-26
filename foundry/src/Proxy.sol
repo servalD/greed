0b1e3abb337be4b833770f41e1842b7e5496f1d1
gaetan cabaret <gaetan@e36knots.com>
2024-11-26 09:41:57 +0100

    fix: struct and import

--- foundry/src/Proxy.sol
+++ foundry/src/Proxy.sol
@@ -7,9 +7,20 @@ contract Proxy {
 
     // Address of the ERC-721 contract to which calls will be forwarded
     address public erc721Contract;
+    // Address of the Safe contract that owns the proxy contract
+    address public owner;
 
-    constructor(address _erc721Contract) {
+    constructor(address _erc721Contract, address safeAddress) {
+        // Set the address of the ERC-721 contract to which calls will be forwarded
         erc721Contract = _erc721Contract;
+        // Set the owner of the proxy contract to the Safe contract
+        owner = safeAddress;
+    }
+
+    // Function to forward calls to the ERC-721 contract
+    function _forwardCall() internal {
+        (bool success,) = erc721Contract.delegatecall(msg.data);
+        require(success, "Delegate call failed");
     }
 
     // Fallback function to forward calls to the erc-721 contract
@@ -22,21 +33,4 @@ contract Proxy {
         _forwardCall();
     }
 
-    // Function to forward calls to the erc-721 contract
-    function _forwardCall() internal {
-        // Forward the call to the erc-721 contract
-        (bool success, bytes memory result) = erc721Contract.call(msg.data);
-
-        // If the call was successful, return the result
-        if (success) {
-            assembly {
-                return(add(result, 32), mload(result))
-            }
-        } else {
-            // If the call was not successful, revert with the error message
-            assembly {
-                revert(add(result, 32), mload(result))
-            }
-        }
-    }
 }
--- foundry/src/collection.sol
+++ foundry/src/collection.sol
@@ -2,23 +2,10 @@
 
 pragma solidity ^0.8.23;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 
 // create erc-721 implementation pointed by a proxy
 contract ERC721Implementation is ERC721 {
     constructor() ERC721("ERC721Implementation", "ERC721IMPL") {}
-}
-
-// create proxy contract
-contract ERC721Proxy is ERC1967Proxy {
-    constructor(address _logic) ERC1967Proxy(_logic) {}
-}
-
-// create proxy factory
-contract ERC721ProxyFactory {
-    function createProxy(address _logic) public returns (ERC721Proxy) {
-        ERC721Proxy proxy = new ERC721Proxy(_logic);
-        return proxy;
-    }
 }
