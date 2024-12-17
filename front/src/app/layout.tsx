72c026bb7d39105d1e81aa9fe480acfb219169f9
Antony Loussararian <antonyloussararian@MacBook-Pro-de-Antony.local>
2024-12-17 17:34:36 +0100

    switch chain

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThirdwebProvider } from "thirdweb/react";
+import { sepolia } from "thirdweb/chains";
 import { WagmiProvider } from 'wagmi'
 import { config } from './client'
 
@@ -21,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <ThirdwebProvider>
+      <ThirdwebProvider activeChain={sepolia}>
         <WagmiProvider config={config}>{children}</WagmiProvider>
       </ThirdwebProvider>
       </body>
