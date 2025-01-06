4a95582cc2c18ddc8c775eae0d10d63026134551
gaetan cabaret <gaetan@e36knots.com>
2025-01-07 00:57:25 +0100

    fix(contract): add test; all pass; missing getter and logic

---
+++ foundry/src/IRoleDefinition.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+abstract contract IRoleDefinition {
+    // Role definitions
+    uint64 public constant AGENCY_ROLE = 1;
+    uint64 public constant AGENT_ROLE = 2;
+    uint64 public constant CLIENT_ROLE = 3;
+    uint64 public constant CO_OWNER_ROLE = 4;
+}
