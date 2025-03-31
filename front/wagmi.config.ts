174f677392ac1c914991c5aebede834037977108
Antony Loussararian <antonyloussararian@MacBook-Pro-de-Antony.local>
2025-03-31 09:58:38 +0200

    type abi

--- front/wagmi.config.ts
+++ front/wagmi.config.ts
@@ -7,13 +7,16 @@ dotenvConf();
 import ManagerABI from '../foundry/out/Manager.sol/Manager.json';
 import AgencyABI from '../foundry/out/Agency.sol/Agency.json';
 import CoproABI from '../foundry/out/Copro.sol/Copro.json';
+import FractionalABI from '../foundry/out/FractionalToken.sol/FractionalToken.json';
+import { type Abi } from 'viem';
 
 export default defineConfig({
   out: 'src/contracts/generatedContracts.ts',
   contracts: [
-    { name: 'Manager', abi: ManagerABI.abi, address: process.env.NEXT_PUBLIC_MANAGER},
-    { name: 'Agency', abi: AgencyABI.abi, address: process.env.NEXT_PUBLIC_AGENCY },
-    { name: 'Copro', abi: CoproABI.abi, address: process.env.NEXT_PUBLIC_COPRO },
+    { name: 'Manager', abi: ManagerABI.abi as Abi, address: process.env.NEXT_PUBLIC_MANAGER as `0x${string}`},
+    { name: 'Agency', abi: AgencyABI.abi as Abi, address: process.env.NEXT_PUBLIC_AGENCY as `0x${string}` },
+    { name: 'Copro', abi: CoproABI.abi as Abi, address: process.env.NEXT_PUBLIC_COPRO as `0x${string}` },
+    { name: 'Fractional', abi: FractionalABI.abi as Abi, address: process.env.NEXT_PUBLIC_FRACTIONAL as `0x${string}` },
   ],
   plugins: [react()],
 })
