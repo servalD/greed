b910c11a658a548054a6a5fe76323fdcb8ad12d4
gaetan cabaret <gaetan@e36knots.com>
2025-07-07 19:39:20 +0200

    fix: for merge
    Signed-off-by: gaetan cabaret <gaetan@e36knots.com>

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 
   return (
     <main className="min-h-screen bg-gradient-to-b from-gray-900 to-black">
-      <Navbar connectionStatus={status} />
+      <Navbar />
       
       {status === "connected" ? (
         <GuestOrClient />
