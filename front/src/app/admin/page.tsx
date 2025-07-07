0fba041fb59b69d1878c1bbe7aacb9de8ec296fa
gaetan cabaret <gaetan@e36knots.com>
2025-07-07 19:39:20 +0200

    fix: for merge
    Signed-off-by: gaetan cabaret <gaetan@e36knots.com>

--- front/src/app/admin/page.tsx
+++ front/src/app/admin/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
 
   return (
     <main>
-      <Navbar connectionStatus={status} />
+      <Navbar />
       {status === "connected" && (role === "agent" || role === 'agency') ? (
         <AgentOrAgency />
       ) : (
